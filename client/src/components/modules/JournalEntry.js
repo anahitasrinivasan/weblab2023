c8859a48f472a094f9010e19f20ed8c0abaac195
Anahita Srinivasan <anahitasrinivasan@dhcp-10-29-11-247.dyn.MIT.EDU>
2023-01-18 11:11:21 -0500

    added JournalEntry.js and .css

---
+++ client/src/components/modules/JournalEntry.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+import "./JournalEntry.css";
+import { post } from "../../utilities";
+
+const JournalEntry = (props) => {
+    const [entryValue, setEntryValue] = useState("");
+    const [sleepValue, setSleepValue] = useState();
+    const [hydrationValue, setHydrationValue] = useState();
+    const [moodValue, setMoodValue] = useState();
+
+    //TO BE REMOVED: simply prints entry values underneath as a test
+    const [textValue, setTextValue] = useState("");
+    
+    // called whenever the user types in the journal entry box
+    const handleEntryChange = (event) => {
+        setEntryValue(event.target.value);
+    };
+
+    // called whenever the user types in how many hours they slept
+    const handleSleepChange = (event) => {
+        setSleepValue(event.target.value);
+    };
+
+    // called whenever the user types in how much water they drank
+    const handleHydrationChange = (event) => {
+        setHydrationValue(event.target.value);
+    };
+
+    // called whenever the user selects a mood from 1-5
+    const handleMoodChange = (event) => {
+        setMoodValue(event.target.value);
+    };
+
+    //called whenever the user hits the submit button
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTextValue(entryValue);
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder={props.defaultEntryText}
+                value={entryValue}
+                onChange={handleEntryChange}
+            />
+            <input 
+                type="number"
+                placeholder={props.defaultSleepText}
+                value={sleepValue}
+                onChange={handleSleepChange}
+            />
+            <input 
+                type="number"
+                placeholder={props.defaultHydrationText}
+                value={hydrationValue}
+                onChange={handleHydrationChange}
+            />
+            <select onChange={handleMoodChange}>
+                <option value="" disabled selected>select mood from 1-5</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+            </select>
+            <button
+                type="submit"
+                value="Submit"
+                onClick={handleSubmit}>
+                Submit
+            </button>
+            <h1>{textValue}</h1>
+        </div>
+    );
+};
+
+export default JournalEntry;
--- client/src/components/pages/Journal.js
+++ client/src/components/pages/Journal.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
+import JournalEntry from "../modules/JournalEntry";
 
 const Journal = (props) => {
     return (
-        <></>
+        <JournalEntry 
+            defaultEntryText="journal entry"
+            defaultSleepText="hours slept"
+            defaultHydrationText="water drank (oz.)"/>
     );
 };
 
