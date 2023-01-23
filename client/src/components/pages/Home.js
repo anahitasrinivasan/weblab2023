bbc87a5076924a7b3fdd1d01462480ba320d03d7
Anahita Srinivasan <anahitasrinivasan@dhcp-10-29-11-247.dyn.mit.edu>
2023-01-23 11:07:08 -0500

    modified some utilies.css content and made home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,14 +7,24 @@ const Home = (props) => {
         <>
             <div className="flex-container">
                 <div className="flex-item">
-                    <img src="https://cdn-icons-png.flaticon.com/512/2917/2917995.png" alt="happy plant!"></img>
-                    <a href="https://www.flaticon.com/free-icons/nature" title="nature icons"
+                    <div className="img-div">
+                    <img src="https://images.unsplash.com/photo-1525923838299-2312b60f6d69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2322&q=80" alt="happy plant!"></img>
+                    </div>
+                    <a href="https://unsplash.com/photos/jt6QxZwSOCQ" title="nature icons"
                     className="image-link u-link">
-                        Nature icons created by Freepik - Flaticon
+                        unsplash
                     </a>
                 </div>
-                <div className="flex-item text">
-                moody is designed to help you keep track of how you’re doing. it keeps your friends in the loop, too, so that you all can thrive together.
+                <div className="flex-item">
+                    <div className="text-header">welcome to moody.</div>
+                    <div className="text">
+                        <h3>to get started: </h3>
+                        login and click on the <u>journal</u> tab to log an entry
+                        <h3>to see your entries: </h3>
+                        click on the <u>profile</u> tab
+                        <h3>to make friends: </h3>
+                        click on the <u>friends</u> tab to add others and view their content
+                    </div>
                 </div>
             </div>
         </>
